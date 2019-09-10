bf897223fc9a6636f5d448e20d710a80c348b791
Nick <nhl1013@gmail.com>
2019-09-10 03:31:49 -0700

    Adding Movie object as input into Movie component to apss values from parent component

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MovieResponse } from 'src/app/models/movie-response';
 
 @Component({
   selector: 'app-movie',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie.component.scss']
 })
 export class MovieComponent implements OnInit {
-
+  @Input() movie: MovieResponse;
   constructor() { }
 
   ngOnInit() {
