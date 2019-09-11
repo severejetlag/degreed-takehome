126494e0e26fe91e16a4475a5ead894d8f0f4488
Nick <nhl1013@gmail.com>
2019-09-10 22:05:01 -0700

    Downloaded backup movie posters if hotloading is not successful. Page will load local image first, if hotloading succeeds then it will hide local image and show api linked image

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input,  } from '@angular/core';
 import { MovieResponse } from 'src/app/models/movie-response';
 import { environment } from 'src/environments/environment';
 
@@ -8,14 +8,22 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./movie.component.scss']
 })
 export class MovieComponent implements OnInit {
+  public backupImageFile: string;
   public imdbMovieURL: string;
+  public imageLoadSuccess = false;
 
   @Input() movie: MovieResponse;
 
   constructor() { }
 
+  public imageLoad($event) {
+    this.imageLoadSuccess = true;
+  }
+
   ngOnInit() {
     this.imdbMovieURL = `${environment.imdbURL}${this.movie.imdbID}`;
+    const fileNamae = this.movie.Poster.substring(this.movie.Poster.lastIndexOf('/'));
+    this.backupImageFile = `assets/images${fileNamae}`;
   }
 
 }
