3331a44b071e522f55da7919abc16bc4053e1d18
Nick <nhl1013@gmail.com>
2019-09-10 23:36:37 -0700

    Updating environments file with HTTPS for omdb api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   omdbKey: 'f3e50227',
   omdbKeyParam: 'apikey=f3e50227',
-  omdbURL: 'http://www.omdbapi.com/',
+  omdbURL: 'https://www.omdbapi.com/',
   imdbURL: 'https://www.imdb.com/title/'
 };
 
